f119ae7015a6ea4c9fe0a78345462963919e5692
oneofourka <oneofourka@mail.ru>
2015-05-30 16:02:41 +1000

    Формы

---
+++ Forms/Autointefication.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MedicalClinic
+{
+    partial class Autointefication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(89, 80);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(100, 20);
+            this.loginBox.TabIndex = 0;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(89, 146);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordBox.TabIndex = 1;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(197, 227);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 2;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // Autointefication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.loginBox);
+            this.Name = "Autointefication";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button enterButton;
+    }
+}
+
---
+++ Forms/Autointefication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MedicalClinic
+{
+    public partial class Autointefication : Form
+    {
+        public Autointefication()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DataManipulator.getUserByName(loginBox.Text).HPWD);
+            if (Model.userExists(loginBox.Text, passwordBox.Text))
+            {
+                if (loginBox.Text != "admin")
+                {
+                    MainUser mainUser = new MainUser();
+                    mainUser.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MainAdmin mainAdmin = new MainAdmin();
+                    mainAdmin.Show();
+                    this.Hide();
+                }
+            }
+            else
+            {
+                MessageBox.Show("error password");
+            }
+        }
+    }
+}

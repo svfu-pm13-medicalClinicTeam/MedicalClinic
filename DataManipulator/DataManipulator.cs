8778498053a177a0155fb99e04318afeabc5de4d
Dmitry Ammosov <dmitryammosov@gmail.com>
2015-05-30 21:09:57 +0900

    В запросе теперь используется prepared statement

--- DataManipulator/DataManipulator.cs
+++ DataManipulator/DataManipulator.cs
@@ -9,22 +9,46 @@ namespace MedicalClinic
 {
     static class DataManipulator
     {
+        static string stringOfConnection = "Server=localhost;Port=5432;User Id=postgres;" +
+                                           "Password=;Database=MedicalClinic";
         public static SoftUser getUserByName(string login)
         {
             SoftUser user = null;
-            string stringOfConnection = "Server=localhost;Port=5432;User Id=postgres;Password=ajdg54;Database=MedicalClinic";
             NpgsqlConnection connection = new NpgsqlConnection(stringOfConnection);
-            NpgsqlCommand command = new NpgsqlCommand("select * from soft_users where login = '"+login+"';",connection);
             connection.Open();
+            NpgsqlCommand command = new NpgsqlCommand("select * from soft_users where login = :login", connection);
+            command.Parameters.Add("login", NpgsqlDbType.Varchar);
+            command.Prepare();
+            command.Parameters[0].Value = login;
             NpgsqlDataReader reader = command.ExecuteReader();
-
-             if (reader.Read())
-             {
+            
+            if (reader.Read())
+            {
                 user = new SoftUser(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-             }
+            }
 
             connection.Close();
             return user;
+        }
+
+        public static List<Doctor> getDoctors(string specialization)
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            NpgsqlConnection connection = new NpgsqlConnection(stringOfConnection);
+            NpgsqlCommand command = new NpgsqlCommand("select * from doctor where specialization = '" + 
+                                                      specialization + "';", connection);
+            connection.Open();
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                doctors.Add(new Doctor(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), 
+                            reader.GetString(3), reader.GetChar(4), reader.GetDateTime(5), reader.GetString(6),
+                            reader.GetString(7), reader.GetString(8), reader.GetString(9)));
+            }
+
+            connection.Close();
+            return doctors;
         }
     }
 }
